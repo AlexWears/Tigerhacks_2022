7137d983c818bb05f8dc2d9f5b820878d7203315
Alex Wears <alexanderwears02@gmail.com>
2022-11-04 20:35:38 -0500

    Settings changes

--- settings.py
+++ settings.py
@@ -4,4 +4,14 @@ import pygame
 class Settings:
 
     def __init__(self):
-        self.bg_color = (86,125,70)
+
+    # Default settings
+
+        self.default_bg_color = (86,125,70)
+
+    # Settings that will NOT change over the course of the game
+        
+        self.bg_color = self.default_bg_color
+
+
+    # Settings that will change over the course of the game
