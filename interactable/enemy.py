03499ce3af2da1c38a2843cca8be7529994af18c
Alex Wears <alexanderwears02@gmail.com>
2022-11-05 12:23:10 -0500

    f

--- interactable/enemy.py
+++ interactable/enemy.py
@@ -8,4 +8,5 @@ class Enemy:
         self.settings = JimRs_Garage.settings
         self.screen = JimRs_Garage.screen
 
-        self.image = 
+        self.image = self.image = pygame.image.load("../sprites/thing.bmp")
+        self.rect = self.image.get_rect
