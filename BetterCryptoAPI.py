2c644af01107259ed8203380a1481438c8b776bf
houghdaniel <hough.daniel.s@gmail.com>
2022-11-04 21:35:13 -0500

    Deleted Jacobs bad crypto API and got a better one

---
+++ BetterCryptoAPI.py
@@ -0,0 +1,6 @@
+import cryptocompare
+
+cryptocompare.cryptocompare._set_api_key_parameter("ab505607f96675cb996706983700a1497b4fa06a9218fc525549f0e50fa0d470")
+
+#print(cryptocompare.get_coin_list(format=True))
+print(cryptocompare.get_price('DESO', currency='USD', full=False))
