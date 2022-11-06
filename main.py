f58e64bcfdbae88a44ab00d666d6961a23fb6990
houghdaniel <hough.daniel.s@gmail.com>
2022-11-06 01:42:39 -0500

    Fixed start button

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ class Goat_Upgrader:
 
 
     def _check_start_button(self, mouse_pos):
-        if(self.start_button.rect.  point(mouse_pos)):
+        if(self.start_button.rect.collidepoint(mouse_pos)):
             self.settings.game_start = True
 
     def clear_screen(self):
@@ -280,11 +280,11 @@ class Goat_Upgrader:
 
         self.start_txt = self.font.render("Press the button or [Enter] to start.", True, self.text_color)
         self.s_txt_rect = self.start_txt.get_rect()
-        self.s_txt_rect.center = (self.settings.width/2, self.settings.height-75)
+        self.s_txt_rect.center = (self.settings.width/2, self.settings.height-70)
 
         self.instructions = self.font.render("Press [Esc] to exit.", True, self.text_color)
         self.inst_rect = self.instructions.get_rect()
-        self.inst_rect.center = (self.settings.width/2, self.settings.height-30)
+        self.inst_rect.center = (self.settings.width/2, self.settings.height-25)
 
         self.screen.blit(self.start_txt, self.s_txt_rect)
         self.screen.blit(self.instructions, self.inst_rect)
