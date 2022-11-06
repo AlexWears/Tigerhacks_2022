b8a0e7141ee9b1201fae30ac5c5f09bd0b01bda9
Alex Wears <alexanderwears02@gmail.com>
2022-11-06 00:48:34 -0500

    Made trees spawn in the right place

--- rock.py
+++ rock.py
@@ -10,4 +10,7 @@ class Rock(Obstacle):
 
         self.rect.x = random.randint(0, self.settings.width - self.rect.width)
 
+        while (self.rect.right > self.settings.road_left) and (self.rect.left < self.settings.road_left + self.settings.road_width):
+            self.rect.x = random.randint(0, self.settings.width - self.rect.width)
+
         self.damage = 45
