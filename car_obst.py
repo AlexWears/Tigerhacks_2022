872ed26f16a816a0ecefb120cabc39dd77496ca1
Alex Wears <alexanderwears02@gmail.com>
2022-11-06 01:00:50 -0500

    adjusted spawn rates

--- car_obst.py
+++ car_obst.py
@@ -7,7 +7,7 @@ class CarObst(Obstacle):
     def __init__(self, Goat_Upgrader, image):
     
         super().__init__(Goat_Upgrader, image)
-        self.speed = self.settings.env_speed + 3
+        self.speed = self.settings.env_speed + 8
         
         if random.randint(1,2) % 2 == 1:
             self.rect.left = int(self.settings.road_left + self.settings.road_width / 4)
